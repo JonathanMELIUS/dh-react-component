dc8e108c33f69439d5fd6e0f9a8d2229ba080fc9
jonathanMELIUS <meliusjonathan@gmail.com>
2023-08-30 13:32:31 +0200

    Update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,18 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 
-import packageJson from "./package.json" assert { type: "json" };
+//NEW
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
-    external: ["react"],
     output: [
       {
         file: packageJson.main,
@@ -23,14 +26,22 @@ export default [
       },
     ],
     plugins: [
+      // NEW
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+
+      // NEW
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
